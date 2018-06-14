97e5a8939cfa8e99c07dd3090cf6da0f275e3fa6
Cameron (3539) <theforgelover@gmail.com>
2018-06-14 18:28:59 -0400

    Tweak Program To Work As Expected
    Fixed response delay
    Fixed left and right being flipped
    Fixed brake and 0 motor speed
    Temp removed watchdog

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -7,11 +7,12 @@ unsigned long currentWatchDogInt;
 
 void setup() 
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.setTimeout(10);
   motor.begin();
 }
 //INFO
-//Right is 1 Left is 0
+//Right is 0 Left is 1
 
 //  Input Data 
 //  Motor Speed = Left:Right
@@ -27,21 +28,18 @@ void loop()
       if(incomingSignal.equals("BRAKE"))
       {
         //BRAKE
-        Serial.println("BRAKE");
-        motor.brake(0);
         motor.brake(1);
+        motor.brake(0);
       }
       else if(incomingSignal.equals("BRAKE R"))
       {
         //BRAKE
-        Serial.println("BRAKE");
-        motor.brake(1);
+        motor.brake(0);
       }
       else if(incomingSignal.equals("BRAKE L"))
       {
         //BRAKE
-        Serial.println("BRAKE");
-        motor.brake(0);
+        motor.brake(1);
       }
       else
       {
@@ -51,8 +49,18 @@ void loop()
           {
             int leftMotorSpeed = incomingSignal.substring(0,colonSpace).toInt();
             int rightMotorSpeed = incomingSignal.substring(colonSpace+1,incomingSignal.length()).toInt();
-            motor.speed(0, leftMotorSpeed); 
-            motor.speed(1, rightMotorSpeed); 
+            motor.speed(1, leftMotorSpeed); 
+            motor.speed(0, rightMotorSpeed); 
+            if(leftMotorSpeed==0)
+            {
+              motor.stop(1);
+              motor.brake(1);
+            }
+            if(rightMotorSpeed==0)
+            {
+              motor.stop(0);
+              motor.brake(0);
+            }
           }
           else
           {
@@ -61,11 +69,11 @@ void loop()
 
       }
     }
-    if (millis()-lastWatchDogInt>1000)
-    {
-      //Stop Motor
-      motor.stop(1);
-      motor.stop(0);
-      Serial.println("STOP!!!!!!");
-    }
+//    if (millis()-lastWatchDogInt>1000)
+//    {
+//      //Stop Motor
+//      motor.stop(1);
+//      motor.stop(0);
+//      Serial.println("STOP!!!!!!");
+//    }
 }
